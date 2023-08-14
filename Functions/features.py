aefcdf500cc3c8cbac0e8ef2fa96ee63312eaf81
Brett Forsen <brettlkforsen@gmail.com>
2023-08-13 21:52:47 -0600

    Added some documentation to the features.py file, tried out some additional metrics for the models.

--- Functions/features.py
+++ Functions/features.py
@@ -1,23 +1,7 @@
 import pandas as pd
 
-# comprehensive_income_df = pd.read_csv('./Data/Cleaned_Pandas_DataFrames/comprehensive_income_df.csv', index_col = [0])
-
-# income_statement_df = pd.read_csv('./Data/Cleaned_Pandas_DataFrames/income_statement_df.csv', index_col = [0])
-
-# cash_flow_df = pd.read_csv('./Data/Cleaned_Pandas_DataFrames/cash_flow_df.csv', index_col = [0])
-
-# balance_sheet_df = pd.read_csv('./Data/Cleaned_Pandas_DataFrames/balance_sheet_df.csv', index_col = [0])
-
-# filing_information_df = pd.read_csv('./Data/Cleaned_Pandas_DataFrames/filing_information_df.csv', index_col = [0])
-
-# company_information_df = pd.read_csv('./Data/Cleaned_Pandas_DataFrames/company_information_df.csv', index_col = [0])
-
-# full_df_list = [comprehensive_income_df, income_statement_df, cash_flow_df, balance_sheet_df, filing_information_df, company_information_df]
-
-# statements_list = [comprehensive_income_df, income_statement_df, cash_flow_df, balance_sheet_df]
-                   
-# other_information_list = [filing_information_df, company_information_df]
-
+# Takes the current assets and current liabilities from the balance sheet to calculate the current ratio.
+# This ratio indicates how readily a company is able to pay off its short-term debt.
 def current_ratio(balance_sheet_df):
     ratio_list = []
     index_list = []
@@ -33,6 +17,9 @@ def current_ratio(balance_sheet_df):
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
+# Takes the net cash flow from the cash flow statement and the current liabilities from the balance sheet.
+# The operating cash flow ratio is used to determine whether or not the company can cover its short-term debt with
+# cash (& cash equivalents) available in the period (in this case annually).
 def operating_cash_flow(cash_flow_df, balance_sheet_df):
     cash_flow_df = cash_flow_df[cash_flow_df.index.isin(balance_sheet_df.index)]
     balance_sheet_df = balance_sheet_df[balance_sheet_df.index.isin(cash_flow_df.index)]
@@ -47,6 +34,9 @@ def operating_cash_flow(cash_flow_df, balance_sheet_df):
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
+# Takes the total liabilities and total equity values from the balance sheet to calculate the debt-to-equity ratio.
+# This is used to give a general sense of how leveraged a company is. Low values trending towards 0 may indicate
+# that the company is underleveraged, whereas excessively high values would imply the opposite.
 def debt_to_equity(balance_sheet_df):
     ratio_list = []
     index_list = []
@@ -59,6 +49,9 @@ def debt_to_equity(balance_sheet_df):
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
+# Takes the company's earnings before income tax as well as the interest expense from the income statement.
+# These are used to determine interest_coverage, which is another indicator of how relatively leveraged a company is.
+# A higher value would indicate that the company's interest payments are manageable on a period to period basis.
 def interest_coverage(income_statement_df):
     ratio_list = []
     index_list = []
@@ -74,23 +67,8 @@ def interest_coverage(income_statement_df):
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
-
-# def asset_turnover(income_statement_df, balance_sheet_df):
-#     income_statement_df = income_statement_df[income_statement_df.index.isin(balance_sheet_df.index)]
-#     balance_sheet_df = balance_sheet_df[balance_sheet_df.index.isin(income_statement_df.index)]
-#     ratio_list = []
-#     index_list = []
-#     for index, row in income_statement_df.iterrows():
-#         net_sales = row['revenues']
-#         # Total assets are used in place of average assets over the period due to inconsistencies between companies.
-#         # The resulting ratios are very similar, and often identical, to what they would have otherwise been.
-#         total_assets = balance_sheet_df.loc[index]['assets'] 
-#         index_list.append(index)
-#         ratio = net_sales / total_assets
-#         ratio_list.append(ratio)
-#     ratio_series = pd.Series(data = ratio_list, index = index_list)
-#     return ratio_series
-
+# Takes both the cost of revenue and total revenues lines from the income statement to calculate operating margin.
+# Operating margin reveals how much profit is made in regards to the cost of generating revenue. 
 def operating_margin(income_statement_df):
     ratio_list = []
     index_list = []
@@ -106,6 +84,8 @@ def operating_margin(income_statement_df):
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
+# Takes net income from the income statement and total assets from the balance sheet to calculate the return on assets.
+# This shows how efficiently a company uses its resources to generate a profit. A higher percentage is better.
 def return_on_assets(income_statement_df, balance_sheet_df):
     income_statement_df = income_statement_df[income_statement_df.index.isin(balance_sheet_df.index)]
     balance_sheet_df = balance_sheet_df[balance_sheet_df.index.isin(income_statement_df.index)]
@@ -120,6 +100,8 @@ def return_on_assets(income_statement_df, balance_sheet_df):
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
+# Takes net income from the income statement and total equity from the balance sheet to calculate return on equity.
+# Similar to return on assets, but due to adding liabilities into the equation it can be seen as more descriptive.
 def return_on_equity(income_statement_df, balance_sheet_df):
     income_statement_df = income_statement_df[income_statement_df.index.isin(balance_sheet_df.index)]
     balance_sheet_df = balance_sheet_df[balance_sheet_df.index.isin(income_statement_df.index)]
@@ -127,17 +109,16 @@ def return_on_equity(income_statement_df, balance_sheet_df):
     index_list = []
     for index, row in income_statement_df.iterrows():
         net_income = row['net_income_loss']
-        total_assets = balance_sheet_df.loc[index]['equity'] 
+        total_equity = balance_sheet_df.loc[index]['equity'] 
         index_list.append(index)
-        ratio = net_income / total_assets
+        ratio = net_income / total_equity
         ratio_list.append(ratio)
     ratio_series = pd.Series(data = ratio_list, index = index_list)
     return ratio_series
 
-# def price_to_earnings(): # Will need to retrieve stock information from another API request
-
+# Conducting some final cleaning on the combined features DataFrame. 
 def final_df_check(feature_df):
-    # Dropping null values and checking each column for ratios of 0, which could be misleading.
+    # Dropping null values and checking each column for ratios of 0, an over abundance of which could be misleading.
     print(feature_df.info())
     feature_df = feature_df.dropna().copy()
     column_list = feature_df.columns
@@ -152,5 +133,4 @@ def final_df_check(feature_df):
         else:
             interest_status.append(0)   
     feature_df['has_interest_payments'] = interest_status
-
     return feature_df
